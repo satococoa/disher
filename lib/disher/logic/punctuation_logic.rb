87703d6386c1e8eac0517cdf3a2cd05b89b37ee9
Satoshi Ebisawa <e.satoshi@gmail.com>
2013-02-13 11:39:07 +0900

    plus_words, minus_words accept regexp

--- lib/disher/logic/punctuation_logic.rb
+++ lib/disher/logic/punctuation_logic.rb
@@ -4,8 +4,8 @@ module Disher
     @@default_options = {
       :punctuations => %w(、 。 ！ ？),
       :extract_tags => %w(div td section article),
-      :plus_words => %w(main),
-      :minus_words => %w(comment amazon.co.jp side menu),
+      :plus_words => [/main/],
+      :minus_words => [/comment/, /side/, /menu/],
       :punctuations_weight => 1.8,
       :children_weight => -0.5,
       :links_weight => -1,
@@ -36,21 +36,19 @@ module Disher
 
     private
     def survey(doc)
-      body = doc.inner_html
-      @punctuations_count = body.count(@options[:punctuations].join)
+      @punctuations_count = doc.text.count(@options[:punctuations].join)
       @children_count     = doc.xpath(".//*").count
       @links_count        = doc.xpath(".//a['href']").count
-      @plus_words_count   = body.count(@options[:plus_words].join)
-      @minus_words_count  = body.count(@options[:minus_words].join)
+      @plus_words_count   = @options[:plus_words].inject(0) {|count, word| count += doc.text.scan(word).count }
+      @minus_words_count  = @options[:minus_words].inject(0) {|count, word| count += doc.text.scan(word).count }
     end
 
     def calculate_score(element)
-      body = element.inner_html
-      punctuations_rate = body.count(@options[:punctuations].join).to_f / @punctuations_count
+      punctuations_rate = element.text.count(@options[:punctuations].join).to_f / @punctuations_count
       children_rate     = element.xpath(".//*").count.to_f / @children_count
       links_rate        = element.xpath(".//a['href']").count.to_f / @links_count
-      plus_words_rate   = body.count(@options[:plus_words].join).to_f / @plus_words_count
-      minus_words_rate  = body.count(@options[:minus_words].join).to_f / @minus_words_count
+      plus_words_rate   = @options[:plus_words].inject(0) {|count, word| count += element.text.scan(word).count }.to_f / @plus_words_count
+      minus_words_rate  = @options[:minus_words].inject(0) {|count, word| count += element.text.scan(word).count }.to_f / @minus_words_count
 
       score = punctuations_rate * @options[:punctuations_weight] +
         children_rate * @options[:children_weight] +
